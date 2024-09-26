1dac61251d56c713df7d240540e7e254b214f06c
Michael Dresler <michaeldresler@hotmail.com>
2024-09-26 16:17:20 -0700

    finished first svg visualization

--- app/iat355/page.tsx
+++ app/iat355/page.tsx
@@ -1,18 +1,67 @@
+const yValues: number[] = [60, 260, 310, 260, 230, 30];
+const scale: number[] = [0, 50, 100, 150, 200, 250, 300];
+const years: string[] = ["1970", "1980", "1990", "2000", "2010", "2020"];
 
-export default function styleguide() {
-    const cells = Array.from({ length: 100 }, (_, index) => (
-        <div key={index} className="flex text-black bg-transparent  w-full h-full hover:bg-white transition-all duration-1000 ">
-          
-        </div>
-      ));
+export default function Styleguide() {
+  return (
+    <div>
+      <main className="w-full h-[46rem] ">
+        <svg width="fill" height="fill" xmlns="http://www.w3.org/2000/svg">
+          <line
+          className="opacity-70"
+            x1="150" // The x-coordinate of the starting point
+            y1="0" // The y-coordinate of the starting point
+            x2="150" // The x-coordinate of the ending point
+            y2="750" // The y-coordinate of the ending point
+            stroke="white" // Color of the line
+            strokeWidth="1" // Thickness of the line
+          />
+          {scale.map((nums: number, index) => (
+            <g className="opacity-60"  key={index}>
+              <text
+               
+                x={100} // Centering text in the middle of the rect
+                y={nums * 2 + 100} // Position text slightly below the bars
+                textAnchor="middle" // Centers the text horizontally
+                style={{ fill: "white", fontSize: "16px" }}
+              >
+                {scale.reverse()[index]}
+              </text>
 
-    return (
-      <div>
-        <main className=" w-full h-screen  ">
-            <div className=" h-full grid grid-cols-10 grid-rows-10">{cells}</div>
-          
-        </main>
-      </div>
-    );
-  }
-  
+              <line
+                x1="140"
+                y1={nums * 2 + 100}
+                x2="800"
+                y2={nums * 2 + 100}
+                stroke="white"
+                strokeWidth="1"
+              />
+            </g>
+          ))}
+          {yValues.map((mydata: number, index) => (
+            <g key={index}>
+              {" "}
+              {/* Grouping rect and text together */}
+              <rect
+                x={index * 90 + 200}
+                y={700 - mydata * 2} // Adjusting y to grow upwards
+                width={75}
+                height={mydata * 2}
+                style={{ fill: "orangered", strokeWidth: 3, stroke: "" }}
+              />
+              <text
+              className="opacity-70"
+                x={index * 90 + 37.5 + 200} // Centering text in the middle of the rect
+                y={720} // Position text slightly below the bars
+                textAnchor="middle" // Centers the text horizontally
+                style={{ fill: "white", fontSize: "16px" }}
+              >
+                {years[index]} {/* Display the corresponding year */}
+              </text>
+            </g>
+          ))}
+        </svg>
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
         <section>
 
         <div className="flex flex-col items-center text-center gap-6">
-          <div className="rounded-full overflow-hidden w-48 h-48  relative border border-white/10 mb-4 bg-white/90">
+          {/* <div className="rounded-full overflow-hidden w-48 h-48  relative border border-white/10 mb-4 bg-white/90">
           <img className="absolute -top-[165%] right-3 w-[1000px] h-[1000px] object-cover " src="/michael.png"  alt="hello" />
-          </div>
+          </div> */}
 
         <h1 className="font-medium text-text-primary md:text-5xl text-2xl tracking-tighter leading-[100%] max-w-[700px] text-pretty ">Hi, I'm Michael Dresler, <br /> Designer and Developer</h1>
         <h2 className="font-light text-[1rem] md: text-lg text-text-secondary max-w-[800px] text-pretty">As a fourth-year Interactive Arts and Technology student, I've developed a strong understanding of UI/UX design and front-end development. I'm eager to apply my skills to create engaging and user-friendly web experiences.</h2>
